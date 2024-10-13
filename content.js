df57d8720e59224d45570f2fe8c70536463079ff
dam-ari <bareket.damari@live.biu.ac.il>
2024-10-14 02:28:23 +0300

    Refactor content.js for auto-fill functionality and options page

--- content.js
+++ content.js
@@ -1,21 +1,33 @@
-console.log('Content script is running');
+console.log("Content script is running");
 
 function fillId() {
   chrome.storage.sync.get("userId", (data) => {
     if (chrome.runtime.lastError) {
-      console.error('Error retrieving userId:', chrome.runtime.lastError);
+      console.error("Error retrieving userId:", chrome.runtime.lastError);
       return;
     }
-    console.log('User ID retrieved:', data.userId);
+    console.log("User ID retrieved:", data.userId);
 
     if (data.userId) {
-      const userIdElements = [document.querySelectorAll("#Ecom_User_Pid"), 
-        document.getElementsByName("id_number")];
-      // the element is the first element in the array that is not an empty array
-    const userIdElement = userIdElements.filter((element) => element.length > 0)[0];
+      // List of selectors to try
+      const selectors = ["#Ecom_User_Pid", "[name='id_number']"];
+      let userIdElement = null;
+
+      // Loop through the selectors
+      for (const selector of selectors) {
+        userIdElement = document.querySelector(selector);
+        if (userIdElement) {
+          break; // Exit the loop if an element is found
+        }
+      }
       if (userIdElement) {
-        userIdElement[0].value = data.userId;
-        console.log("Filled user ID", data.userId, "into element", userIdElement);
+        userIdElement.value = data.userId;
+        console.log(
+          "Filled user ID",
+          data.userId,
+          "into element",
+          userIdElement
+        );
         const loginButton = document.querySelector("#loginButton");
         if (loginButton) {
           loginButton.click();
